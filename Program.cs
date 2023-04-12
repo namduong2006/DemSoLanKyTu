858f41b54ae71b50d9ffe654d77d68b62e1b1c6c
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-12 21:14:31 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemSoLanKyTu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Chuoi();
+            Console.ReadLine();
+        }
+        static void Chuoi()
+        {
+            string n, t;
+            int dem = 0, dai;
+            Console.WriteLine("Nhập chuỗi (KHÔNG DẤU) muốn kiểm tra:");
+            n = Console.ReadLine();
+            Console.WriteLine("Chuỗi vừa tạo:");
+            char[] chuoi = n.ToCharArray();
+            dai = chuoi.Length;
+            for (int i = 0; i < dai; i++)
+            {
+                Console.Write($"{chuoi[i]}|");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Nhập 1 ký tự muốn tìm:");
+            t = Console.ReadLine();
+            char[] tim = t.ToCharArray();
+            for (int i = 0; i < dai; i++)
+            {
+                if (tim[0] == chuoi[i])
+                {
+                    dem++;
+                }
+            }
+            Console.WriteLine("Số lần xuất hiện của ký tự trong chuỗi là: " + dem);
+        }
+    }
+}
